d5bbbc3c8dd00eea328fcce0a2ca33e30e877ce4
Rafael Cruz <ralph1786@gmail.com>
2018-11-27 20:06:23 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,5 +1,6 @@
 def run_guessing_game
   players_input = ""
   while players_input
-  puts "Please guess a number between 1 and 10"
+  puts "Please guess a number between 1 and 10:"
+  players_input = gets.chomp
 end
