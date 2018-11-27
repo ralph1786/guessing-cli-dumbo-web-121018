5373f586e13bbad95352b60d3a148e48a21be91a
Rafael Cruz <ralph1786@gmail.com>
2018-11-27 20:27:21 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,7 +2,7 @@ def run_guessing_game
   players_input = ""
   while players_input
     puts "Please guess a number between 1 and 10:"
-    players_input = gets.downcase.chomp
+    players_input = gets.chomp
     rand_number = rand(1..10).to_s
     if players_input == rand_number
       puts "You guessed the correct number!"
